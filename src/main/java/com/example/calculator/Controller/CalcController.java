8802c27c461f6ccba6519ff89dbae07964bf5a51
Poli18na <isip_p.s.hutornaya@mpt.ru>
2022-09-16 23:49:22 +0300

    test commit

--- src/main/java/com/example/calculator/Controller/CalcController.java
+++ src/main/java/com/example/calculator/Controller/CalcController.java
@@ -5,7 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+//test commit
 @Controller
 public class CalcController {
     @GetMapping("/")
